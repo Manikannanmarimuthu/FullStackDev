6838ee3fe48e31c0a2959082342020f3428b8c2e
Manikannan Marimuthu <mani@Manikannans-MacBook-Pro.local>
2021-06-05 13:32:24 +0530

    Completed Day1 Task

--- FullStackDev/src/com/practice/day1/Calculator.java
+++ FullStackDev/src/com/practice/day1/Calculator.java
@@ -3,6 +3,14 @@ package com.practice.day1;
 public class Calculator {
 
 	public static void main(String[] args) {
+		System.out.println("Length is :: " + args.length);
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c= a+b;
+		int d= a-b;
+		System.out.println("Addition Operation outut is :: " + "" + c);
+		System.out.println("Subraction Operation outut is :: " + "" + d);
+		System.out.println("Multiplication Operation outut is :: " + a * b);
 
 	}
 
