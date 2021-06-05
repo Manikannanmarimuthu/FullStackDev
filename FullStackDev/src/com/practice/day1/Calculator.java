93e98f4773e1d8841b0930cd923a05116a2b557b
Manikannan Marimuthu <mani@Manikannans-MacBook-Pro.local>
2021-06-05 13:38:53 +0530

    Removed unused variables

--- FullStackDev/src/com/practice/day1/Calculator.java
+++ FullStackDev/src/com/practice/day1/Calculator.java
@@ -6,11 +6,9 @@ public class Calculator {
 		System.out.println("Length is :: " + args.length);
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
-		int c= a+b;
-		int d= a-b;
-		System.out.println("Addition Operation outut is :: " + "" + c);
-		System.out.println("Subraction Operation outut is :: " + "" + d);
-		System.out.println("Multiplication Operation outut is :: " + a * b);
+		System.out.println("Addition Operation outut is :: " +(a+b));
+		System.out.println("Subraction Operation outut is :: " + (a-b));
+		System.out.println("Multiplication Operation outut is :: " + (a * b));
 
 	}
 
