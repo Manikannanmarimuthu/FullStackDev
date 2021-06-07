28fec587e58e3c56ee5e446f34529f96194a06e0
mani <mani@192.168.1.5>
2021-06-07 19:07:32 +0530

    NumberAddition Completed

--- FullStackDev/src/com/practice/qn05/StringToNumber.java
+++ FullStackDev/src/com/practice/qn05/StringToNumber.java
@@ -3,8 +3,17 @@ package com.practice.qn05;
 public class StringToNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String[] StrArr = { "2AA", "12", "ABC", "67H", "uu678" };
+		int total = 0;
+		for (int i = 0; i < StrArr.length; i++) {
+			String str = StrArr[i];
+			for (int j = 0; j < str.length(); j++) {
+				if (Character.isDigit(str.charAt(j))) {
+					total = total + Integer.parseInt(String.valueOf(str.charAt(j)));
+				}
+			}
+		}
+		System.out.println("Total " + total);
 	}
-
 }
